41c8d48b18f249721d00f8f36a01fc9d3dc6e4fb
Lee Morgan <me@leemorgan.io>
2023-05-31 16:54:31 -0400

    Create route for uploading a repo.

---
+++ repo.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+let RepoSchema = new mongoose.Schema({
+    link: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: String,
+        required: true
+    },
+    repo: {
+        type: String,
+        required: true
+    },
+    uuid: {
+        type: String,
+        required: true
+    },
+    notes: {
+        type: String,
+        required: false
+    }
+})
+
+module.exports = mongoose.model("repo", RepoSchema);
--- routes.js
+++ routes.js
@@ -1,3 +1,37 @@
+const Repo = require("./repo.js");
+
+const {exec} = require("child_process");
+const uuid = require("crypto").randomUUID;
+
 module.exports = (app)=>{
-    console.log("something");
+    /*
+    POST: upload GitHub repo from entered url
+    req.body = {
+        url: Repo link
+        module: Number
+        notes: String
+    }
+    response = Repo
+    */
+    app.post("/upload", async (req, res)=>{
+        let repo = await Repo.findOne({link: req.body.url});
+        if(repo !== null) return res.json("Repo already archived");
+        let linkParts = req.body.url.split("/");
+        let id = uuid();
+        let cloneCommand = `git clone ${req.body.url} ${__dirname}/repos/module${req.body.module}/${id}`;
+
+        exec(cloneCommand, async (err, stdout, stderr)=>{
+            let newRepo = new Repo({
+                link: req.body.url,
+                user: linkParts[3],
+                repo: linkParts[4],
+                uuid: id,
+                notes: req.body.notes ? req.body.notes : ""
+            });
+
+            await newRepo.save();
+
+            res.json(newRepo);
+        });
+    });
 }
