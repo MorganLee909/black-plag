ae794b73996f0811e2da41977839fdad3d750561
Lee Morgan <me@leemorgan.io>
2023-05-31 13:29:06 -0400

    Create entry file.

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const compression = require("compression");
+
+const app = express();
+
+let mongooseOptions = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+};
+
+if(process.env.NODE_ENV === "production"){
+    mongooseOptions.auth = {authSource: "admin"};
+    mongooseOptions.user = "website";
+    mongooseOptions.pass = process.env.MONGODB_PASS;
+}
+
+mongoose.connect("mongodb://127.0.0.1/plag", mongooseOptions);
+
+app.use(compression());
+app.use(express.json());
+
+require("./routes.js")(app);
+
+if(process.env.NODE_ENV === "production"){
+    module.exports = app;
+}else{
+    app.listen(process.env.PORT);
+}
