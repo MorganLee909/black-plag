407179ce10d0693858902d16354cb5d38a37b8d1
Lee Morgan <me@leemorgan.io>
2023-05-31 18:46:03 -0400

    Tried stuff, got nowhere.

--- search.js
+++ search.js
@@ -1,9 +1,11 @@
 const {exec} = require("child_process");
 
 let searchDir = (dir, snippet)=>{
-    let command = `pcregrep -FlMr '${snippet}' ${dir}`;
+    let command = `pcregrep -Flr "${snippet}" ${dir}`;
 
+    console.log(command);
     exec(command, (err, stdout, stderr)=>{
+        console.log("something");
         console.log(err);
         console.log(stdout);
         console.log(stderr);
