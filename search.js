67a4c9d30476ec3f0202b30e8bf58f455ba6b384
Lee Morgan <me@leemorgan.io>
2023-06-01 10:21:28 -0400

    Frontend updated to upload repos.

--- search.js
+++ search.js
@@ -1,15 +1,9 @@
 const {exec} = require("child_process");
 
-let searchDir = (dir, snippet)=>{
+module.exports = (dir, snippet)=>{
     let command = `pcregrep -Flr "${snippet}" ${dir}`;
 
-    console.log(command);
     exec(command, (err, stdout, stderr)=>{
-        console.log("something");
-        console.log(err);
         console.log(stdout);
-        console.log(stderr);
     });
 }
-
-module.exports = searchDir;
