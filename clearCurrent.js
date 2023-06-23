f5b653a62e1dec26184fcd0f59090b2c861e4ad1
Lee Morgan <me@leemorgan.io>
2023-06-23 14:49:30 -0400

    Add file to retroactively remove uneccessary files.

---
+++ clearCurrent.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const recurseDirectory = require("helper.js");
+
+let removeFile = (filePath)=>{
+    if(
+        filePath.includes("node_modules") ||
+        filePath.includes(".git") ||
+        filePath.includes("package-lock.json") ||
+        filePath.includes(".jpg") ||
+        filePath.includes(".jpeg") ||
+        filePath.includes(".webp") ||
+        filePath.includes(".png") ||
+        filePath.includes(".gif") ||
+        filePath.includes(".svg") ||
+        filePath.includes(".ico") ||
+        filePath.includes(".webm") ||
+        filePath.includes(".mkv") ||
+        filePath.includes(".avi") ||
+        filePath.includes(".mov") ||
+        filePath.includes(".wmv") ||
+        filePath.includes(".mp4") ||
+        filePath.includes(".m4p") ||
+        filePath.includes(".m4v") 
+    ){
+        fs.rm(filePath, {}, (err)=>{});
+    }
+}
+
+recurseDirectory(`${__dirname}/repos`, removeFile);
