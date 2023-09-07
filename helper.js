7d58382ae2ccfbe90885c74028f603f9066b6990
Lee Morgan <me@leemorgan.io>
2023-09-07 03:02:12 -0400

    Add function to clone the repository.

--- helper.js
+++ helper.js
@@ -1,21 +1,46 @@
 const fs = require("fs");
 const uuid = require("crypto").randomUUID;
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const Repo = require("./repo.js");
 
-let recurseDirectory = (path, cb)=>{
-    fs.readdir(path, (err, files)=>{
-        for(let i = 0; i < files.length; i++){
-            let newPath = `${path}/${files[i]}`;
-            cb(newPath);
-            try{
-                if(fs.lstatSync(newPath).isDirectory()) recurseDirectory(newPath, cb);
-            }catch(e){}
-        }
-    });
+const recurseDirectory = (path, cb)=>{
+    let files = fs.readdirSync(path);
+
+    for(let i = 0; i < files.length; i++){
+        let newPath = `${path}/${files[i]}`;
+        cb(newPath, repo);
+        try{
+            if(fs.lstatSync(newPath).isDirectory()) recurseDirectory(newPath, cb, repo);
+        }catch(e){}
+    }
 },
 
-const cloneRepo = awync (mod, link)=>{
+const removeFiles = (filePath)=>{
+    if(
+        filePath.includes("node_modules") ||
+        filePath.includes(".git") ||
+        filePath.includes("package-lock.json") ||
+        filePath.includes(".jpg") ||
+        filePath.includes(".jpeg") ||
+        filePath.includes(".webp") ||
+        filePath.includes(".png") ||
+        filePath.includes(".gif") ||
+        filePath.includes(".svg") ||
+        filePath.includes(".ico") ||
+        filePath.includes(".webm") ||
+        filePath.includes(".mkv") ||
+        filePath.includes(".avi") ||
+        filePath.includes(".mov") ||
+        filePath.includes(".wmv") ||
+        filePath.includes(".mp4") ||
+        filePath.includes(".m4p") ||
+        filePath.includes(".m4v") 
+    ) fs.rm(filePath, {recursive: true, force: true}, (err)=>{});
+}
+
+const cloneRepo = async (mod, link)=>{
     link = link.trim();
     let archivedLink = link.replace(".git", "");
 
@@ -25,7 +50,10 @@ const cloneRepo = awync (mod, link)=>{
 
     let cloneCommand = `git clone ${link} ${__dirname}/repos/module${mod}/${id}`;
 
-    exec(cloneCommand, async)
+    let {err} = await exec(cloneCommand);
+    if(err) throw new Error("Unable to clone repository");
+
+    recurseDirectory(`${__dirname}/repos/module${mod}/${id}`, removeFiles);
 }
 
 module.exports = {
