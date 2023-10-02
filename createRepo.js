932c0d3b3841272181808aa9374a6eedd02f0718
Lee Morgan <me@leemorgan.io>
2023-10-02 11:16:45 -0400

    Refactor file deletion to only keep necessary files.

--- createRepo.js
+++ createRepo.js
@@ -7,26 +7,21 @@ const exec = util.promisify(require("child_process").exec);
 const fs = require("fs");
 
 const removeFiles = (filePath)=>{
-    if(
-        filePath.includes("node_modules") ||
-        filePath.includes(".git") ||
-        filePath.includes("package-lock.json") ||
-        filePath.includes(".jpg") ||
-        filePath.includes(".jpeg") ||
-        filePath.includes(".webp") ||
-        filePath.includes(".png") ||
-        filePath.includes(".gif") ||
-        filePath.includes(".svg") ||
-        filePath.includes(".ico") ||
-        filePath.includes(".webm") ||
-        filePath.includes(".mkv") ||
-        filePath.includes(".avi") ||
-        filePath.includes(".mov") ||
-        filePath.includes(".wmv") ||
-        filePath.includes(".mp4") ||
-        filePath.includes(".m4p") ||
-        filePath.includes(".m4v") 
-    ) fs.rm(filePath, {recursive: true, force: true}, (err)=>{});
+    let fileName = filePath.split("/");
+    fileName = fileName[fileName.length-1];
+    fileName = fileName.toLowerCase();
+
+    if(fileName === "build" || fileName === "dist"){
+        fs.rm(filePath, {recursive: true, force: true}, (err)=>{});
+    }else if(
+        !fileName.includes(".js") &&
+        !fileName.includes(".css") && !fileName.includes(".html") &&
+        !fileName.includes(".jsx") &&
+        !fileName.includes(".md") &&
+        !fileName.includes(".handlebars")
+    ){
+        fs.rm(filePath, {}, (err)=>{})
+    }
 }
 
 const cloneRepo = async (mod, link)=>{
